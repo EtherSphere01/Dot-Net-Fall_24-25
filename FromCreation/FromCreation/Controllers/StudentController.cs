c5fbf1d0c5aff8ac3e2c45be734658bcc8e070ba
Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2024-11-02 00:21:47 +0600

    Initial commit

---
+++ FromCreation/FromCreation/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using FromCreation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FromCreation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+       public ActionResult Form()
+        {
+            return View(new StudentInfo());
+        }
+
+        [HttpPost]
+        public ActionResult Form(StudentInfo student)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+           
+            return View(student);
+        }
+
+    }
+}
---
+++ FromCreation/FromCreation/Models/StudentInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace FromCreation.Models
+{
+    public class StudentInfo
+    {
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[a-zA-z.-]+$", ErrorMessage = "Name must contain only alphabets, (.) and (-)")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage = "Id must follow XX-XXXXX-X where X is number")]
+        public string Id { get; set; }
+
+        [Required]
+        [CustomDob(20)]
+        public string Dob { get; set; }
+
+        [Required]
+        [CustomEmail]
+        public string Email { get; set; }
+
+        //internal static string ReferenceEquals(string v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+  
+}
