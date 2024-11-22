9ca73f74f7724810cb645c3da4a55ef13c299038
Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2024-11-22 21:23:18 +0600

    update

---
+++ Demo_program/Demo_program/Validation/CustomValidaton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Demo_program.Validation
+{
+    public class CustomValidaton : ValidationAttribute
+    {
+
+    }
+}
