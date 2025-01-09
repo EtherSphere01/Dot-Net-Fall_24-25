7370aa45157cf7ae597b646ddcc41477cfdeac92
Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2025-01-09 22:19:01 +0600

    test

---
+++ Intro3Tier/WebApplication1/DAL/Repos/StudentRepo.cs
@@ -0,0 +1,24 @@
+﻿using DAL.EF;
+using DAL.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class StudentRepo
+    {
+        NewsContext db;
+        public StudentRepo() { 
+
+           db = new NewsContext();  
+        }
+        public List<Student> GetStudent()
+        {
+            var data = db.Students.ToList();
+            return data;
+        }
+    }
+}
---
+++ Intro3Tier/WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : ApiController
+    {
+        StudentService service = new StudentService();
+
+        [HttpGet]
+        [Route("api/student/getstudent")]
+        public HttpResponseMessage GetStudent()
+        {
+            var data = service.GetStudent();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+    }
+}
