95fe97974c39c31f7f65bc60c62189d695f0eb58
Md. Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2025-01-20 14:09:43 +0600

    final project

---
+++ Final_Project/News Aggregator/DAL/EF/Tables/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Tables
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content {  get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Tag { get; set; }
+
+
+    }
+}
